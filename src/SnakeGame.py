d10fd223f396dc1dc4fb67138c9d61ef38255fe5
Victoria Hantgin <victoriaelleh@gmail.com>
2025-04-30 18:18:18 -0500

    created Fruit class, wrote simple planning psuedocode

--- src/SnakeGame.py
+++ src/SnakeGame.py
@@ -1,14 +1,22 @@
 import pygame, sys
 
+class FRUIT:
+    def __init__(self):
+        # crearte an x and y pos
+        # draw a square
+
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size)) #size of window
 clock = pygame.time.Clock()
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    screen.fill((175,215,70)) #screen color
     pygame.display.update()
     clock.tick(60) #framerate
-    
