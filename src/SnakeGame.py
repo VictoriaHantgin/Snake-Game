56de1c4a6227079e2e2441d6fbf3f7e1788a4625
Victoria Hantgin <victoriaelleh@gmail.com>
2025-04-30 18:31:58 -0500

    set up fruit class completely, made the position of the fruit random, implemented drawing the fruit.

--- src/SnakeGame.py
+++ src/SnakeGame.py
@@ -1,9 +1,16 @@
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
 
 class FRUIT:
     def __init__(self):
-        # crearte an x and y pos
-        # draw a square
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = pygame.math.Vector2(self.x,self.y)
+       
+    def draw_fruit(self):
+       fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+       pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+       
 
 pygame.init()
 cell_size = 40
@@ -11,6 +18,7 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size)) #size of window
 clock = pygame.time.Clock()
 
+fruit = FRUIT()
 
 while True:
     for event in pygame.event.get():
@@ -18,5 +26,6 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill((175,215,70)) #screen color
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60) #framerate
