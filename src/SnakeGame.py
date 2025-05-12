f64da777a198038e3dd72e1ea25c0ff41b512ddb
Victoria Hantgin <victoriaelleh@gmail.com>
2025-05-12 14:42:43 -0500

    created a fix to the bug where the lives would contiously subtract before movement of the snake happens. However, this may only be a temporary fix. I will see.

--- src/SnakeGame.py
+++ src/SnakeGame.py
@@ -165,30 +165,23 @@ class MAIN:
             self.power.spawn()
             self.lives = self.lives + 1
             
-
-
     def check_fail(self):
         # check if the snake is outside the screen
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
-            self.lives = self.lives - 1
             self.game_over()
         #check if snake hits itself
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
-                self.lives = self.lives - 1
                 self.game_over()
        
     def game_over(self):
-
         if self.lives > 0:
+            self.lives = self.lives - 1
             self.snake.reset()
         else:
             #return to main menu
             self.snake.reset()
             
-    
-        #pygame.quit()
-        #sys.exit()
     
     def draw_grass(self):
         grass_color = (167,209,61) 
@@ -219,7 +212,7 @@ class MAIN:
         pygame.draw.rect(screen,(56,74,12),bg_rect,2) #frame of score box 2 is the line width
     
     def draw_life(self):
-        life_text = str((self.lives))
+        life_text = str((self.lives + 1))
         life_surface = game_font.render(life_text,True,(56,74,12))
         life_x = int(cell_size * cell_number - 730)
         life_y = int(cell_size * cell_number - 40)
